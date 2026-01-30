825e48e83e30172a8369702e4151f68b2bbf0517
Davi Bernardo <dnssites@gmail.com>
2026-01-30 20:41:13 -0300

    Variaveis

---
+++ fundamentos-do-python/variaveis-iteracao-do-usuario.py
@@ -0,0 +1,8 @@
+# função
+# print() - Saída de dados (mostra na tela)
+# type() - verifica do tipo de dados (mostra o tipo de dados)
+# input() - entrada de dados (interage com o usuário)
+
+nome = input('Digite o seu nome: ')
+idade = input('Digite a sua idade: ')
+print('olá ', nome+',', ' você tem ', idade, 'de idade')
---
+++ fundamentos-do-python/variaveis.py
@@ -0,0 +1,9 @@
+nome = 'Davi Bernado' # String
+idade = 41 # Integer
+altura = 1,70 # Float
+professor = True # Boolean
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(professor))
